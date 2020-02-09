4515158d335f163d6f1609b650e4fa148202ea11
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-02-09 21:39:13 +0000

    add new activities, upgrade main layout, link buttons

---
+++ app/src/main/java/com/example/autenticatorapp/FloorPlan.java
@@ -0,0 +1,14 @@
+package com.example.autenticatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FloorPlan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floor_plan);
+    }
+}
--- app/src/main/java/com/example/autenticatorapp/MainActivity.java
+++ app/src/main/java/com/example/autenticatorapp/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button timetables, library, moodle, floor_plan, moreBtn;
     FirebaseAuth fAuth;
 
     @Override
@@ -21,6 +23,49 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(getApplicationContext(), Register.class));
             finish();
         }
+
+        timetables = findViewById(R.id.timetables);
+        library = findViewById(R.id.library);
+        moodle = findViewById(R.id.moodle);
+        floor_plan = findViewById(R.id.floor_plan);
+        moreBtn = findViewById(R.id.moreBtn);
+
+        timetables.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Timetables.class));
+            }
+        });
+        library.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Library.class));
+            }
+        });
+
+        moodle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // pass
+            }
+        });
+
+        floor_plan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),FloorPlan.class));
+            }
+        });
+
+        moreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),More.class));
+            }
+        });
+
+
+
 
     }
     public void logout (View view){
