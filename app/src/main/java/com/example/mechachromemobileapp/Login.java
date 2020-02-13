f4791e6ef66685e86714b319a4987c8f77cd1d2f
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-02-13 23:25:29 +0000

    finished login with studentID

--- app/src/main/java/com/example/mechachromemobileapp/Login.java
+++ app/src/main/java/com/example/mechachromemobileapp/Login.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,13 +18,21 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class Login extends AppCompatActivity {
 
+    public static final String TAG = "TAG";
     EditText mStudentID,mPassword;
     Button mLoginBtn;
     TextView mCreateBtn;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
     ProgressBar progressBar;
 
 
@@ -35,7 +44,11 @@ public class Login extends AppCompatActivity {
         mStudentID = findViewById(R.id.studentID);
         mPassword = findViewById(R.id.password);
         mLoginBtn = findViewById(R.id.loginBtn);
+
+        fStore = FirebaseFirestore.getInstance();
         fAuth = FirebaseAuth.getInstance();
+
+
         progressBar = findViewById(R.id.progressBar);
         mCreateBtn = findViewById(R.id.registerBtn);
 
@@ -43,10 +56,10 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String email = mStudentID.getText().toString().trim();
-                String password = mPassword.getText().toString().trim();
+                String studentID = mStudentID.getText().toString().trim();
+                final String password = mPassword.getText().toString().trim();
 
-                if(TextUtils.isEmpty(email)){
+                if(TextUtils.isEmpty(studentID)){
                     mStudentID.setError("studentID is Required.");
                     return;
                 }
@@ -65,19 +78,38 @@ public class Login extends AppCompatActivity {
 
                 // authenticate the user
 
-                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                DocumentReference documentReference = fStore.collection("ids").document(studentID);
+                documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                        }else {
-                            Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            progressBar.setVisibility(View.GONE);
-                        }
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if(document.exists()) {
+                                Log.d(TAG,"Got the email");
+                                final String email = document.get("email").toString();
+                                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if(task.isSuccessful()){
+                                            Toast.makeText(Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                        }else {
+                                            Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                            progressBar.setVisibility(View.GONE);
+                                        }
 
+                                    }
+                                });
+                            } else {
+                                Log.d(TAG, "No such studentID");
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
                     }
                 });
+
+
 
             }
         });
--- app/src/main/java/com/example/mechachromemobileapp/User.java
+++ app/src/main/java/com/example/mechachromemobileapp/User.java
@@ -1,4 +1,25 @@
 package com.example.mechachromemobileapp;
 
 public class User {
+
+    public String studentID, fname, lname, email;
+
+    public User() {
+
+    }
+
+    public User(String studentID, String fname, String lname, String email) {
+        this.studentID = studentID;
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+    }
+
+    public String getStudentID(){
+        return studentID;
+    }
+    public String getEmail(){
+        return email;
+    }
+
 }
