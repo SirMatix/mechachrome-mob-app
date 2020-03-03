aa3cd18c4f96af7e66ea994306db7e4f21026a21
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-03-03 18:45:56 +0000

    add book page logic

--- app/src/main/java/com/example/mechachromemobileapp/BookPage.java
+++ app/src/main/java/com/example/mechachromemobileapp/BookPage.java
@@ -4,11 +4,34 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class BookPage extends AppCompatActivity {
+
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_page);
+
+    }
+
+    public void loadBookData() {
+        fStore = FirebaseFirestore.getInstance();
+
+
+    }
+
+    public void loadReviews() {
+
+    }
+
+    public void reserveBook() {
+
+    }
+
+    public void writeReview() {
+
     }
 }
