a968ddb7bfd5774c2a4c6641435a55a92a03eeb3
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-03-17 14:45:46 +0000

    Add drawables

---
+++ app/src/main/java/com/example/mechachromemobileapp/Activities/BookSale/BookSale.java
@@ -0,0 +1,14 @@
+package com.example.mechachromemobileapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookSale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_sale);
+    }
+}
