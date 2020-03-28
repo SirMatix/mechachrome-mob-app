7d7a06dffdb87bce9dd638b31159859c37707a3a
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-03-28 16:55:13 +0000

    Add bookSalePage activity

---
+++ app/src/main/java/com/example/mechachromemobileapp/Activities/BookSale/BookSalePage.java
@@ -0,0 +1,16 @@
+package com.example.mechachromemobileapp.Activities.BookSale;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mechachromemobileapp.R;
+
+public class BookSalePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_sale_page);
+    }
+}
