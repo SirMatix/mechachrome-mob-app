13841f28352da2224010639f695f38c106be087c
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-04-02 12:10:29 +0100

    BookSalePage done, changes to main activity

--- app/src/main/java/com/example/mechachromemobileapp/Activities/BookSale/BookSalePage.java
+++ app/src/main/java/com/example/mechachromemobileapp/Activities/BookSale/BookSalePage.java
@@ -1,24 +1,99 @@
 package com.example.mechachromemobileapp.Activities.BookSale;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.example.mechachromemobileapp.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class BookSalePage extends AppCompatActivity {
+
+    public static final String TAG = "TAG";
+    FirebaseFirestore fStore;
+    FirebaseAuth fAuth;
+    String bookSaleIDFeed, titleFeed;
+    Button buyBookButton, messageSellerButton;
+    TextView bookTitle, bookAuthor, bookDescription, bookPages, bookPrice, bookCondition;
+    ImageView bookImage;
+    CollectionReference bookSaleCollection;
+    DocumentReference bookSaleReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_sale_page);
+
+        initViews();
+        loadBookData();
+
     }
 
-    public void initViews() {
+    public void initViews(){
+        fStore = FirebaseFirestore.getInstance();
+        fAuth = FirebaseAuth.getInstance();
+        bookSaleCollection = fStore.collection("library_books");
 
+        // getting intent from Library activity and getting extra string
+        Intent intent = getIntent();
+        bookSaleIDFeed = intent.getStringExtra("book_id");
+        titleFeed = intent.getStringExtra("book_title");
+
+        // finding variables from layout
+        bookTitle = findViewById(R.id.item_book_title);
+        bookAuthor = findViewById(R.id.item_book_author);
+        bookDescription = findViewById(R.id.availableBooks);
+        bookPages = findViewById(R.id.item_book_pagesrev);
+        bookPrice = findViewById(R.id.book_price);
+        bookImage = findViewById(R.id.item_book_img);
+        bookCondition = findViewById(R.id.item_book_condition);
+
+        // buttons
+        messageSellerButton = findViewById(R.id.write_to_seller_button);
+        buyBookButton = findViewById(R.id.buy_book_button);
+    }
+
+    public void loadBookData() {
+        bookSaleReference = bookSaleCollection.document(bookSaleIDFeed);
+        bookSaleReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                bookTitle.setText(documentSnapshot.getString("title"));
+                bookAuthor.setText(documentSnapshot.getString("author"));
+                bookDescription.setText(documentSnapshot.getString("description"));
+                String pages = documentSnapshot.get("pages").toString();
+                String displayPages = pages + " Pages";
+                bookPages.setText(displayPages);
+                bookCondition.setText(documentSnapshot.getString("condition"));
+                bookPrice.setText(documentSnapshot.getString("price"));
+
+                Glide.with(getApplicationContext())
+                        .load(documentSnapshot.get("imgUrl").toString()) //set the img book url
+                        .transforms(new CenterCrop(), new RoundedCorners(16))
+                        .into(bookImage); //destination path
+            }
+        });
     }
 
     public void buyBook() {
 
     }
+
+    public void messageSeller() {
+
+    }
+
 }
--- app/src/main/java/com/example/mechachromemobileapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/mechachromemobileapp/Activities/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.mechachromemobileapp.Activities.BookSale.BookSale;
 import com.example.mechachromemobileapp.Activities.Forum.Forum;
 import com.example.mechachromemobileapp.Activities.Library.LibraryAdmin;
 import com.example.mechachromemobileapp.Activities.Library.LibraryUser;
@@ -34,7 +35,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "TAG";
-    Button timetables, library, moodle, floor_plan, moreBtn;
+    Button timetables, library, moodle, floor_plan, moreBtn, bookSaleBtn;
     String userID;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
@@ -56,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         moodle = findViewById(R.id.moodle);
         floor_plan = findViewById(R.id.floor_plan);
         moreBtn = findViewById(R.id.moreBtn);
+        bookSaleBtn = findViewById(R.id.book_sale);
 
         timetables.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,6 +94,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        bookSaleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), BookSale.class));
+            }
+        });
+
     }
 
     @Override
@@ -113,6 +122,9 @@ public class MainActivity extends AppCompatActivity {
             case R.id.user_settings:
                 userSettings();
                 return true;
+            case R.id.user_logout:
+                logout();
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -129,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(), UserSettings.class));
     }
 
-    public void logout (View view){
+    public void logout (){
         FirebaseAuth.getInstance().signOut(); //logout
         startActivity(new Intent(getApplicationContext(), Login.class));
         finish();
