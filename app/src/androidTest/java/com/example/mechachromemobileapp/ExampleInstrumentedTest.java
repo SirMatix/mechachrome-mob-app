f75ef46177c7baae50210ea8ee90990fd5a79fba
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-04-08 01:52:57 +0100

    corrected app name in Instrumented Test

--- app/src/androidTest/java/com/example/mechachromemobileapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mechachromemobileapp/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.autenticatorapp", appContext.getPackageName());
+        assertEquals("com.example.mechachromemobileapp", appContext.getPackageName());
     }
 }
